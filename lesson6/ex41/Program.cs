6086d99243c2a90ae97bd3274834f7a81469aa4f
Shved Anna <pandatek@inbox.ru>
2022-07-20 10:10:13 +0300

    homework 6

---
+++ lesson6/ex41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.Write("Enter count of numbers (M): ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayM = new int[M];
+for(int i = 0; i < M; i++) {
+    Console.Write("Enter " + (i+1) + " number of array: ");
+    arrayM[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("[" + string.Join(", ", arrayM) + "]");
+Console.WriteLine();
+
+int CountOfPositiveNumber(int[] arr) {
+    int count = 0;
+    for(int j = 0; j < arr.Length; j++) {
+        if(arr[j] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Count of even numbers: " + CountOfPositiveNumber(arrayM));
---
+++ lesson6/ex43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Enter k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x1 = 0;
+double y1 = k1 * x1 + b1;
+
+double x2 = 0;
+double y2 = k2 * x2 + b2;
+
+double  x = 0, y = 0;
+
+x = (b2 - b1) / (k1 - k2);
+y = k1 * x + b1;
+
+Console.WriteLine("x = {0}", + x);
+Console.WriteLine("y = {0}", + y);
