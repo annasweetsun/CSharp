9eb3e261c6f0348f65bdbba90efdfd86d7550d16
Shved Anna <pandatek@inbox.ru>
2022-07-20 10:21:48 +0300

    Homework 1

---
+++ lesson1/ex01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number A:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b * b == a) {
+    Console.WriteLine("Yes");
+}
+else {
+    Console.WriteLine("No");
+}
---
+++ lesson1/ex02/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter number of week`s day");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1) Console.WriteLine("Monday");
+else {
+    if (day == 2) Console.WriteLine("Tuesday");
+    else {
+        if (day == 3) Console.WriteLine("Wednesday");
+        else {
+            if (day == 4) Console.WriteLine("Thursday");
+            else {
+                if (day == 5) Console.WriteLine("Friday");
+                else {
+                    if (day == 6) Console.WriteLine("Saturday");
+                    else {
+                        if (day == 7) Console.WriteLine("Sunday");
+                        else {
+                            Console.WriteLine("There is not a day of the week");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
