a865ccd6a946d8c352468e99c365cc771c83c410
Shved Anna <pandatek@inbox.ru>
2022-07-20 09:53:41 +0300

    Homework 2

---
+++ ex10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Write("Enter three-digit number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+//a = a % 100 / 10;
+Console.Write(a % 100 / 10);
---
+++ ex15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Enter the number of day of the week: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 7) Console.WriteLine("There are seven days of the week.");
+else if (a < 1) Console.WriteLine("There are seven days of the week.");
+// так и не поняла, почему не работает составное условие и разделила на два, но
+else {
+    if (a > 5) Console.WriteLine("Day off. You can relax.");
+    else {
+        Console.WriteLine("It is not day off. Go to the work.");
+    }
+    
+}
